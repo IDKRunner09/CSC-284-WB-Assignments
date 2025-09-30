d8f3412b5dcf1ebda9d40d32ab71c834871124ec
IDKRunner09 <spatel190905@gmail.com>
2025-09-30 16:13:36 -0400

    Update MemoryLeak.cpp

--- Unit_03_01_MemoryLeak/MemoryLeak.cpp
+++ Unit_03_01_MemoryLeak/MemoryLeak.cpp
@@ -16,7 +16,7 @@ int main() {
 		printf("%d sec \t1 mb of memory allocated\n", i);
 		this_thread::sleep_for(chrono::seconds(1));
 	}
-	/
+	// the for looop below deletes the memory
 	for (size_t i = 0; i < memory.size(); i++) {
 		delete[] memory[i];
 		this_thread::sleep_for(chrono::seconds(1));
